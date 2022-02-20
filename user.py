96ad85e988ee9633d6119d94a28cac4bde5e1831
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-02-20 08:52:58 +0300

    it will generate the class user that will store as well as define the user interface

---
+++ user.py
@@ -0,0 +1,13 @@
+class User:
+    '''
+    It creates the user class that generates new instances of the user
+    '''
+    user_list = []
+
+    def _init_(self,username,password);
+        
+        '''
+        this method will define all the properties that the user needs to have i.e username and password
+        '''
+        self.username = username
+        self.password = password
