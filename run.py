452fcbaaf2479fe695cd18cea10e2bd197714f90
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-02-21 04:49:25 +0300

    it will craete a function that will create a new user

--- run.py
+++ run.py
@@ -1,1 +1,18 @@
 from user import User, Secretdetails
+
+def function():
+        print("")
+        print("")
+        print("")
+        print("")
+        print("")
+        print("")
+function()
+
+def create_newuser(username, password):
+    '''
+    this is function that is used to create a new username and a password
+    '''
+
+    newuser = User(username, password)
+    return newuser
